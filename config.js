5498fce3e0183ff0147ac54477bb2e805682c9d4
Dikaa-rizii <112540130+Dikaa-rizii@users.noreply.github.com>
2022-11-06 12:18:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Nandzyy'
 global.Anticall = false
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
+global.owner = ['628998823028']
 global.premium = ['60177426853']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
